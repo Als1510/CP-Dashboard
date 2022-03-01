028c078642dcd04505e17884b86c6f9ffc4090c8
Als1510 <lekhrajsharmaas@gmail.com>
2022-03-01 15:09:27 +0530

    feat: Add forget password

--- Frontend/src/app/services/env.service.ts
+++ Frontend/src/app/services/env.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class EnvService {
 
-  Mongo_API_URL = 'http://localhost:5000/api'
-  // Mongo_API_URL = 'https://competitivepdashboard.herokuapp.com/api'
+  // Mongo_API_URL = 'http://localhost:5000/api'
+  Mongo_API_URL = 'https://competitivepdashboard.herokuapp.com/api'
   Contest_API_URL = 'https://kontests.net/api/v1/all';
 
   constructor() { }
