bf80c4229c6b9999ae0d7947475c5cfb69b60bb8
Als1510 <lekhrajsharmaas@gmail.com>
2022-03-10 21:06:19 +0530

    feat: Add contact form

--- Frontend/src/app/pages/user/contact-us/contact-us.page.ts
+++ Frontend/src/app/pages/user/contact-us/contact-us.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertService } from 'src/app/services/alert.service';
+import { NetlifyFormService } from 'src/app/services/netlify-form.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -13,7 +14,8 @@ export class ContactUsPage implements OnInit {
   contactForm: FormGroup
   constructor(
     private _formBuilder: FormBuilder,
-    private _alertService: AlertService
+    private _alertService: AlertService,
+    private _netlifyFormService: NetlifyFormService
   ) { }
 
   ngOnInit() {
@@ -26,15 +28,12 @@ export class ContactUsPage implements OnInit {
   }
 
   onSubmit() {
-    let data = `name: ${this.contactForm.get('name').value}, email: ${this.contactForm.get('email').value}, email: ${this.contactForm.get('email').value}, message: ${this.contactForm.get('message').value}`
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(data).toString(),
-    })
-      .then(() => this._alertService.presentToast('We\'ll contact you soon', 'success'))
-      .catch((error) => this._alertService.presentToast('error', 'danger'));
-    this.contactForm.reset()
+    this._netlifyFormService.submitFeedback(this.contactForm.value).subscribe(
+      ()=> {
+        this.contactForm.reset();
+        this._alertService.presentToast('We\'ll contact you soon', 'success')
+      }
+    )
   }
 
 }
---
+++ Frontend/src/app/pages/utils/contactUs.ts
@@ -0,0 +1,6 @@
+export interface contactUs{
+  name: string;
+  email: string;
+  contactNo: number;
+  message: string;
+}
