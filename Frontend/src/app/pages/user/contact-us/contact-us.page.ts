cdf6101303c957ebabf895eedd1cb977c867aa53
Als1510 <lekhrajsharmaas@gmail.com>
2022-03-10 20:49:44 +0530

    feat: Add contact form

--- Frontend/src/app/pages/user/contact-us/contact-us.page.ts
+++ Frontend/src/app/pages/user/contact-us/contact-us.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -12,6 +13,7 @@ export class ContactUsPage implements OnInit {
   contactForm: FormGroup
   constructor(
     private _formBuilder: FormBuilder,
+    private _alertService: AlertService
   ) { }
 
   ngOnInit() {
@@ -24,14 +26,14 @@ export class ContactUsPage implements OnInit {
   }
 
   onSubmit() {
+    let data = `name: ${this.contactForm.get('name').value}, email: ${this.contactForm.get('email').value}, email: ${this.contactForm.get('email').value}, message: ${this.contactForm.get('message').value}`
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(this.contactForm.value).toString(),
+      body: new URLSearchParams(data).toString(),
     })
-      .then(() => console.log("Form successfully submitted"))
-      .catch((error) => alert(error));
-      console.log(this.contactForm.value)
+      .then(() => this._alertService.presentToast('We\'ll contact you soon', 'success'))
+      .catch((error) => this._alertService.presentToast('error', 'danger'));
     this.contactForm.reset()
   }
 
